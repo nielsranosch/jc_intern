8e79d8d3fec58893b27244342abd224ce1f8da3c
Niels Ranosch <niels@ranosch.de>
2020-02-08 16:40:49 +0100

    Disabled client-side caching

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         CheckForMaintenanceMode::class,
+        \App\Http\Middleware\CacheControl::class
     ];
 
     /**
